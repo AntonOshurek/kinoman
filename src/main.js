7814d924616f55751184bb548092472873cb3e81
AntonOshurek <oshurekweb@gmail.com>
2022-03-06 19:28:27 +0100

    orrection functions

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
+import { render } from './services/render';
+
+//views
 import { createFooterTemplate } from './view/footer';
 import { createProfileTemplate } from './view/profile';
 import { createNavigationTemplate } from './view/navigation';
 import { createSortTemplate } from './view/sort';
-
 import { createFilmsTemplate } from './view/films';
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const siteFooterStatistics = document.querySelector('.footer__statistics');
 const siteHeader = document.querySelector('.header');
 const siteMain = document.querySelector('.main');
 
-render(siteFooterStatistics, createFooterTemplate(), 'beforeend');
 render(siteHeader, createProfileTemplate(), 'beforeend');
 render(siteMain, createNavigationTemplate(), 'beforeend');
 render(siteMain, createSortTemplate(), 'beforeend');
 render(siteMain, createFilmsTemplate(), 'beforeend');
+render(siteFooterStatistics, createFooterTemplate(), 'beforeend');
