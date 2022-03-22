0cf6297cf9376ae13ebf893e40c85a669ea5c168
AntonOshurek <oshurekweb@gmail.com>
2022-03-22 13:04:57 +0100

    add new film view and optimyze code

--- src/view/films-list.js
+++ src/view/films-list.js
@@ -1,44 +1,20 @@
-import { generateFilm } from '../mock/mock';
+import { createFilmTemplate } from './film';
 
-export const createFilmsListTemplate = () => {
+export const createFilmsListTemplate = (filmsArray) => {
 
-  const generateOneFilm = (film) => {
-    const {title, description, poster, comments, rating} = film;
-    return `
-      <article class="film-card">
-        <h3 class="film-card__title">${title}</h3>
-        <p class="film-card__rating">${rating}</p>
-        <p class="film-card__info">
-          <span class="film-card__year">1929</span>
-          <span class="film-card__duration">1h 55m</span>
-          <span class="film-card__genre">Musical</span>
-        </p>
-        <img src="./images/posters/${poster}" alt="" class="film-card__poster">
-        <p class="film-card__description">${description}</p>
-        <a class="film-card__comments">${comments.length}</a>
-        <div class="film-card__controls">
-          <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-          <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-          <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
-        </div>
-      </article>
-    `;
+  const generateFilmsList = () => {
+    let filmsList = '';
+    filmsArray.forEach((film) => {
+      filmsList += createFilmTemplate(film);
+    });
+    return filmsList;
   };
-
-  const FILMS_COUNT = 12;
-  const filmsArray = Array.from({length: FILMS_COUNT}, generateFilm);
-
-  let filmsList = '';
-
-  filmsArray.forEach((film) => {
-    filmsList += generateOneFilm(film);
-  });
 
   return `
     <section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
       <div class="films-list__container">
-        ${filmsList}
+        ${generateFilmsList()}
       </div>
       <button class="films-list__show-more">Show more</button>
     </section>
--- src/view/films.js
+++ src/view/films.js
@@ -2,10 +2,15 @@ import { createFilmsListTemplate } from './films-list';
 import { createFilmsListTopTemplate } from './films-list-top';
 import { createFilmsListCommentedTemplate } from './films-list-commented';
 
+import { generateFilm } from '../mock/mock';
+
+const FILMS_COUNT = 12;
+const filmsArray = Array.from({length: FILMS_COUNT}, generateFilm);
+
 export const createFilmsTemplate = () => (
   `
     <section class="films">
-      ${createFilmsListTemplate()}
+      ${createFilmsListTemplate(filmsArray)}
       ${createFilmsListTopTemplate()}
       ${createFilmsListCommentedTemplate()}
     </section>
