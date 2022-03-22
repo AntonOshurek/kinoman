c4e6c94700aec9d1908c62aca33f13351a20060a
AntonOshurek <oshurekweb@gmail.com>
2022-03-22 18:17:19 +0100

    delete unused code in movies list

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { render } from './services/render';
 
-//views
+//main views
 import { createFooterTemplate } from './view/footer';
 import { createProfileTemplate } from './view/profile';
 import { createNavigationTemplate } from './view/navigation';
 import { createSortTemplate } from './view/sort';
 import { createFilmsTemplate } from './view/films';
-
+//films views
 import { createFilmsListTemplate } from './view/films-list';
 import { createFilmsListTopTemplate } from './view/films-list-top';
 import { createFilmsListCommentedTemplate } from './view/films-list-commented';
 import { createFilmTemplate } from './view/film';
 import { createLoadMoreButton } from './view/loadMoreButton';
-
+//data
 import { generateFilm } from './mock/mock';
 
 const siteFooterStatistics = document.querySelector('.footer__statistics');
@@ -35,6 +35,7 @@ render(siteFilms, createFilmsListCommentedTemplate(), 'beforeend');
 
 const FILMS_COUNT = 12;
 const FILMS_COUNT_PER_STEP = 5;
+
 const filmsArray = Array.from({length: FILMS_COUNT}, generateFilm);
 const siteFilmsList = document.querySelector('.films-list');
 const siteFilmsListContainer = document.querySelector('.films-list__container');
@@ -63,7 +64,6 @@ if (filmsArray.length > FILMS_COUNT_PER_STEP) {
     }
   });
 }
-
 
 // footer block
 render(siteFooterStatistics, createFooterTemplate(), 'beforeend');
--- src/view/films-list.js
+++ src/view/films-list.js
@@ -1,22 +1,8 @@
-// import { createFilmTemplate } from './film';
-
 export const createFilmsListTemplate = () => `
-    <section class="films-list">
-      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-      <div class="films-list__container">
+  <section class="films-list">
+    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    <div class="films-list__container">
 
-      </div>
-    </section>
-  `;
-
-// <div class="films-list__container">
-// ${filmsList}
-// </div>
-// <button class="films-list__show-more">Show more</button>
-
-
-// let filmsList = '';
-
-// filmsArray.forEach((film) => {
-//   filmsList += createFilmTemplate(film);
-// });
+    </div>
+  </section>
+`;
