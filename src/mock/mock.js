5ca6839ec94a73010ef42725872fc75c5c4c3c09
AntonOshurek <oshurekweb@gmail.com>
2022-03-23 19:56:55 +0100

    add new gener data template

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,4 +1,5 @@
 import { fishText } from '../../node_modules/fish-text/fish-text.js';
+import dayjs from 'dayjs';
 import { getRandomInt, getUNID } from '../utils/utils';
 
 const POSTERS = [
@@ -47,8 +48,8 @@ const getComments = () => {
   for(let i = 0; i < getRandomInt(1, 12); i++) {
     comments.push({
       'id': getUNID(),
-      'author': fishText.getNames({count: 1, type: 'full', lang: 'eng', dataType: 'string'}),
-      'comment': fishText.getRandomRangeWords({min: 3, max: 12, dataType: 'string', lang: 'eng', repeat: false}),
+      'author': fishText.getNames({count: 1, type: 'full'}),
+      'comment': fishText.getRandomRangeWords({min: 3, max: 12}),
       'emotion': EMOTIONS[getRandomInt(0, EMOTIONS.length - 1)],
     });
   }
@@ -56,11 +57,56 @@ const getComments = () => {
 };
 
 export const generateFilm = () => ({
-  title: fishText.getRandomRangeWords({min: 1, max: 4, dataType: 'string', lang: 'eng', repeat: false}),
-  description: fishText.getRandomRangeWords({min: 10, max: 25, dataType: 'string', lang: 'eng', repeat: false}),
+  title: fishText.getRandomRangeWords({min: 1, max: 4}),
+  description: fishText.getRandomRangeWords({min: 10, max: 25}),
   poster: getRandomPoster(),
   rating: `${getRandomInt(1, 9)  }.${  getRandomInt(0, 9)}`,
   year: getRandomInt(1940, 2010),
   genre: GENRE[getRandomInt(0, GENRE.length - 1)],
   comments: getComments(),
 });
+
+const generateRandomDate = () => {
+  const bar = getRandomInt(0, -26000);
+  return dayjs().add(bar, 'day').toDate();
+};
+
+const fooBar = {
+  'id': '0',
+  'comments': [
+    'id1', 'id2',
+  ],
+  'film_info': {
+    'title': fishText.getRandomRangeWords({min: 1, max: 4}),
+    'altertive_title': fishText.getRandomRangeWords({min: 4, max: 6}),
+    'total_rating': `${getRandomInt(1, 9)  }.${  getRandomInt(0, 9)}`,
+    'poster': getRandomPoster(),
+    'age_rating': getRandomInt(0, 18),
+    'director': fishText.getNames({count: 1, type: 'full'}),
+    'writers': [
+      fishText.getNames({count: 1, type: 'full'}),
+    ],
+    'actors': [
+      fishText.getNames({count: 1, type: 'full'}),
+      fishText.getNames({count: 1, type: 'full'}),
+      fishText.getNames({count: 1, type: 'full'}),
+    ],
+    'release': {
+      'date': generateRandomDate(),
+      'release_country': fishText.getCountries({}),
+    },
+    'runtime': getRandomInt(40, 180),
+    'genre': [
+      GENRE[getRandomInt(0, GENRE.length - 1)],
+    ],
+    'description': fishText.getRandomRangeWords({min: 10, max: 25}),
+    'user_details': {
+      'watchlist': false,
+      'already_watched': true,
+      'watching_date': generateRandomDate(),
+      'favorite': false,
+    },
+  },
+};
+
+console.log(fooBar);
