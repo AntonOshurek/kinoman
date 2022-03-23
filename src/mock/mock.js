38ca9303819ba532dc7b597f83e401900a0cfcca
AntonOshurek <oshurekweb@gmail.com>
2022-03-23 21:03:52 +0100

    add comments UNID and array for comments

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,6 +1,5 @@
 import { fishText } from '../../node_modules/fish-text/fish-text.js';
-import dayjs from 'dayjs';
-import { getRandomInt, getUNID } from '../utils/utils';
+import { getRandomInt, getUNID, generateRandomDate, booleanGenerate } from '../utils/utils';
 
 const POSTERS = [
   'made-for-each-other.png',
@@ -37,43 +36,48 @@ const EMOTIONS = [
   'angry',
 ];
 
+const commentsArray = [];
+const commentsUNIDArray = [];
+
+const COMMENTS_COUNT = 100;
+
+for(let i = 0; i < COMMENTS_COUNT; i++) {
+  commentsUNIDArray.push(getUNID());
+}
+
+const generateComment = (unid) => (
+  {
+    'id': unid,
+    'author': fishText.getNames({count: 1, type: 'full'}),
+    'comment': fishText.getRandomRangeWords({min: 3, max: 12}),
+    'date': generateRandomDate(-5000),
+    'emotion': EMOTIONS[getRandomInt(0, EMOTIONS.length - 1)],
+  }
+);
+
+for(let i = 0; i < COMMENTS_COUNT; i++) {
+  const commentUNID = commentsUNIDArray[i];
+  const getOneComment = generateComment(commentUNID);
+  commentsArray.push(getOneComment);
+}
+
 const getRandomPoster = () => {
   const filmPoster = POSTERS[getRandomInt(0, POSTERS.length - 1)];
   return filmPoster;
 };
 
-const getComments = () => {
+const getCommentsID = () => {
   // eslint-disable-next-line prefer-const
-  let comments = [];
+  let commentsID = [];
   for(let i = 0; i < getRandomInt(1, 12); i++) {
-    comments.push({
-      'id': getUNID(),
-      'author': fishText.getNames({count: 1, type: 'full'}),
-      'comment': fishText.getRandomRangeWords({min: 3, max: 12}),
-      'emotion': EMOTIONS[getRandomInt(0, EMOTIONS.length - 1)],
-    });
+    commentsID.push(commentsUNIDArray[getRandomInt(0, commentsUNIDArray.length - 1)]);
   }
-  return comments;
-};
-
-// export const generateFilm = () => ({
-//   title: fishText.getRandomRangeWords({min: 1, max: 4}),
-//   description: fishText.getRandomRangeWords({min: 10, max: 25}),
-//   poster: getRandomPoster(),
-//   rating: `${getRandomInt(1, 9)  }.${  getRandomInt(0, 9)}`,
-//   year: getRandomInt(1940, 2010),
-//   genre: GENRE[getRandomInt(0, GENRE.length - 1)],
-//   comments: getComments(),
-// });
-
-const generateRandomDate = () => {
-  const bar = getRandomInt(0, -26000);
-  return dayjs().add(bar, 'day').toDate();
+  return commentsID;
 };
 
 export const generateFilm = () => ({
-  'id': '0',
-  'comments': getComments(),
+  'id': getUNID(),
+  'comments': getCommentsID(),
   'film_info': {
     'title': fishText.getRandomRangeWords({min: 1, max: 4}),
     'altertive_title': fishText.getRandomRangeWords({min: 4, max: 6}),
@@ -90,7 +94,7 @@ export const generateFilm = () => ({
       fishText.getNames({count: 1, type: 'full'}),
     ],
     'release': {
-      'date': generateRandomDate(),
+      'date': generateRandomDate(-26000),
       'release_country': fishText.getCountries({}),
     },
     'runtime': getRandomInt(40, 180),
@@ -99,10 +103,10 @@ export const generateFilm = () => ({
     ],
     'description': fishText.getRandomRangeWords({min: 10, max: 25}),
     'user_details': {
-      'watchlist': false,
-      'already_watched': true,
-      'watching_date': generateRandomDate(),
-      'favorite': false,
+      'watchlist': booleanGenerate(),
+      'already_watched': booleanGenerate(),
+      'watching_date': generateRandomDate(-5000),
+      'favorite': booleanGenerate(),
     },
   },
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 export const getRandomInt = (minValue, maxValue) => Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 
 export const getUNID = () => (
@@ -8,3 +10,12 @@ export const sortByField = (field) => {
   const result = (a, b) => a[field] < b[field] ? 1 : -1;
   return result;
 };
+
+export const generateRandomDate = (range) => {
+  const bar = getRandomInt(0, range);
+  return dayjs().add(bar, 'day').toDate();
+};
+
+export const booleanGenerate = () => (
+  Boolean(getRandomInt(0, 1))
+);
