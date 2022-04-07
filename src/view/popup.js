ce308f8675adff673cea7eda3272826b8c7d4d44
AntonOshurek <oshurekweb@gmail.com>
2022-04-07 17:43:07 +0200

    add show userDetails info in popup

--- src/view/popup.js
+++ src/view/popup.js
@@ -2,7 +2,9 @@ import AbstractView from './abstract-view';
 import { dateFormater } from '../utils/date';
 
 const createpopupTemplate = (filmData, commentsArray) => {
-  const {film_info: filmInfo } = filmData;
+  const {film_info: filmInfo, user_details: userDetails } = filmData;
+  const {watchlist, already_watched: alredyWatched, favorite} = userDetails;
+  console.log(userDetails)
 
   const generateFilmComments = () => {
     let commentsMarkup = '';
@@ -108,9 +110,9 @@ const createpopupTemplate = (filmData, commentsArray) => {
       </div>
 
       <section class="film-details__controls">
-        <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
-        <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
-        <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
+        <button type="button" class="film-details__control-button film-details__control-button--watchlist ${watchlist ? 'film-details__control-button--active' : ''}" id="watchlist" name="watchlist">Add to watchlist</button>
+        <button type="button" class="film-details__control-button film-details__control-button--watched ${alredyWatched ? 'film-details__control-button--active' : ''}" id="watched" name="watched">Already watched</button>
+        <button type="button" class="film-details__control-button film-details__control-button--favorite ${favorite ? 'film-details__control-button--active' : ''}" id="favorite" name="favorite">Add to favorites</button>
       </section>
     </div>
 
