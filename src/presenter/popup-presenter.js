4f8e174f1678024b866157840bcdd9c550718fab
AntonOshurek <oshurekweb@gmail.com>
2022-04-07 16:20:03 +0200

    correction popup rpesenter

--- src/presenter/popup-presenter.js
+++ src/presenter/popup-presenter.js
@@ -1,33 +1,44 @@
 import PopupView from '../view/popup';
 
+import { SITE_BODY, SITE_MAIN, RenderPosition } from '../utils/constants';
+import { remove, render } from '../utils/render';
+import { onEscKeyDown } from '../utils/common';
+
 export default class PopupPresenter {
   constructor() {
-    this.popupComponent = null;
+    this._popupComponent = null;
+    this._defaultFilmsArray = [];
+    this._commentsArray = [];
+  }
+
+  init(defaultFilmsArray, commentsArray) {
+    this._defaultFilmsArray = defaultFilmsArray;
+    this._commentsArray = commentsArray;
   }
 
   _closePopup() {
     SITE_BODY.classList.remove('hide-overflow');
-    remove(popupComponent);
-    popupComponent = null;
+    remove(this._popupComponent);
+    this._popupComponent = null;
     document.removeEventListener('keydown', onEscKeyDown);
-    siteFilmsView.setOpenPopupClikHandler(openPopup);
+    // siteFilmsView.setOpenPopupClikHandler(this._openPopup);
   }
 
   _openPopup(evt) {
     if(evt.target.closest('.film-card')) {
       //search current film
       const filmUNID = evt.target.closest('.film-card').getAttribute('data-unid');
-      const currentFilm = defaultFilmsArray.find((film) => film.id === filmUNID);
+      const currentFilm = this._defaultFilmsArray.find((film) => film.id === filmUNID);
       //generate popup markup
-      popupComponent = new PopupView(currentFilm, commentsArray);
+      this._popupComponent = new PopupView(currentFilm, this._commentsArray);
       //show popup
-      render(SITE_MAIN, popupComponent, RenderPosition.BEFOREEND);
+      render(SITE_MAIN, this._popupComponent, RenderPosition.BEFOREEND);
 
       SITE_BODY.classList.add('hide-overflow'); //hide scroll
-      siteFilmsView.removeOpenPopupClikHandler();
+      // siteFilmsView.removeOpenPopupClikHandler();
       //listeners for closed popup
-      popupComponent.setClosePopupButtonClickHandler(closePopup);
-      document.addEventListener('keydown', () => onEscKeyDown(evt, closePopup()));
+      this._popupComponent.setClosePopupButtonClickHandler(this._closePopup);
+      document.addEventListener('keydown', () => onEscKeyDown(evt, this._closePopup()));
     }
   }
 }
