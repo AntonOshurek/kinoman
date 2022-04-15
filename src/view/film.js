901481a3f9333bf58c3db8e9a763173c1e0242c9
AntonOshurek <oshurekweb@gmail.com>
2022-04-15 18:16:25 +0200

    refactoring filmView and add parse data function

--- src/view/film.js
+++ src/view/film.js
@@ -2,35 +2,24 @@ import AbstractView from './abstract-view';
 import { dateFormater, transformRuntime } from '../utils/date';
 
 const createFilmTemplate = (filmData) => {
-  const {comments, id, film_info: filmInfo, user_details: userDetails } = filmData;
-  const formatDate = dateFormater(filmInfo.release.date);
-
-  const checkFilmDesriptionLength = () => {
-    if(filmInfo.description.length >= 140) {
-      const sliceString = filmInfo.description.slice(0, 139);
-      const result = `${sliceString}...`;
-      return result;
-    } else {
-      return filmInfo.description;
-    }
-  };
+  const {id, title, totalRating, releaseDate, runtime, genre, poster, description, commentsLength, watchList, alredyWatched, favorite} = filmData;
 
   return `
     <article class="film-card" data-unid=${id}>
-      <h3 class="film-card__title">${filmInfo.title}</h3>
-      <p class="film-card__rating">${filmInfo.total_rating}</p>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${totalRating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">${formatDate}</span>
-        <span class="film-card__duration">${transformRuntime(filmInfo.runtime)}</span>
-        <span class="film-card__genre">${filmInfo.genre.join(', ')}</span>
+        <span class="film-card__year">${releaseDate}</span>
+        <span class="film-card__duration">${runtime}</span>
+        <span class="film-card__genre">${genre}</span>
       </p>
-      <img src="./images/posters/${filmInfo.poster}" alt="" class="film-card__poster">
-      <p class="film-card__description">${checkFilmDesriptionLength()}</p>
-      <a class="film-card__comments">${comments.length}</a>
+      <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+      <p class="film-card__description">${description}</p>
+      <a class="film-card__comments">${commentsLength}</a>
       <div class="film-card__controls">
-        <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${userDetails.watchlist ? 'film-card__controls-item--active' : ''}" name="watchlist" type="button">Add to watchlist</button>
-        <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${userDetails.already_watched ? 'film-card__controls-item--active' : ''}" name="already_watched" type="button">Mark as watched</button>
-        <button class="film-card__controls-item film-card__controls-item--favorite ${userDetails.favorite ? 'film-card__controls-item--active' : ''}" name="favorite" type="button">Mark as favorite</button>
+        <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${watchList ? 'film-card__controls-item--active' : ''}" name="watchlist" type="button">Add to watchlist</button>
+        <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${alredyWatched ? 'film-card__controls-item--active' : ''}" name="already_watched" type="button">Mark as watched</button>
+        <button class="film-card__controls-item film-card__controls-item--favorite ${favorite ? 'film-card__controls-item--active' : ''}" name="favorite" type="button">Mark as favorite</button>
       </div>
     </article>
   `;
@@ -40,14 +29,42 @@ export default class Film extends AbstractView {
   constructor(filmData) {
     super();
     this._filmData = filmData;
+    this._convertedFilmData = this._transformDataForView();
 
     this._watchlistClickHandler = this._watchlistClickHandler.bind(this);
     this._watchedClickHandler = this._watchedClickHandler.bind(this);
     this._favoriteClickHandler = this._favoriteClickHandler.bind(this);
   }
 
+  _checkFilmDesriptionLength() {
+    if(this._filmData.film_info.description.length >= 140) {
+      const sliceString = this._filmData.film_info.description.slice(0, 139);
+      const result = `${sliceString}...`;
+      return result;
+    } else {
+      return this._filmData.film_info.description;
+    }
+  }
+
+  _transformDataForView() {
+    return {
+      id: this._filmData.id,
+      title: this._filmData.film_info.title,
+      totalRating: this._filmData.film_info.total_rating,
+      releaseDate: dateFormater(this._filmData.film_info.release.date),
+      runtime: transformRuntime(this._filmData.film_info.runtime),
+      genre: this._filmData.film_info.genre.join(', '),
+      poster: this._filmData.film_info.poster,
+      description: this._checkFilmDesriptionLength(),
+      commentsLength: this._filmData.comments.length,
+      watchList: this._filmData.user_details.watchlist,
+      alredyWatched: this._filmData.user_details.already_watched,
+      favorite: this._filmData.user_details.favorite,
+    };
+  }
+
   getTemplate() {
-    return createFilmTemplate(this._filmData);
+    return createFilmTemplate(this._convertedFilmData);
   }
 
   _watchlistClickHandler(evt) {
