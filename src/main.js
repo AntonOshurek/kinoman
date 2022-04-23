cd9689245ee7579be9193a5a3d098561ec1aa15d
AntonOshurek <oshurekweb@gmail.com>
2022-04-23 22:23:26 +0200

    add films model to main

--- src/main.js
+++ src/main.js
@@ -5,14 +5,19 @@ import { FILMS_COUNT } from './utils/constants';
 import ProfileView from './view/profile';
 import FooterView from './view/footer';
 import { generateFilm, commentsArray } from './mock/mock';
+const films = Array.from({length: FILMS_COUNT}, generateFilm);
 //presenters
 import FilmsBoardPresenter from './presenter/films-board';
+// models
+import Films from './model/films-model';
 
-const defaultFilmsArray = Array.from({length: FILMS_COUNT}, generateFilm);
+const filmsModel = new Films();
+filmsModel.setFilms(films);
+filmsModel.setComments(commentsArray);
 
 render(SITE_HEADER, new ProfileView(), RenderPosition.BEFOREEND);
 
 const filmsBoardPresenter = new FilmsBoardPresenter();
-filmsBoardPresenter.init(defaultFilmsArray, commentsArray);
+filmsBoardPresenter.init(films, commentsArray);
 
-render(SITE_FOOTER_STATISTICS, new FooterView(+defaultFilmsArray.length), RenderPosition.BEFOREEND);
+render(SITE_FOOTER_STATISTICS, new FooterView(filmsModel.getFilms().length), RenderPosition.BEFOREEND);
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -4,13 +4,22 @@ export default class Films extends AbstractObserver {
   constructor() {
     super();
     this._films = [];
+    this._comments = [];
   }
 
   setFilms(films) {
     this._films = films.slice();
   }
 
+  setComments(comments) {
+    this._comments = comments;
+  }
+
   getFilms() {
     return this._films;
+  }
+
+  getComments() {
+    return this._comments;
   }
 }
