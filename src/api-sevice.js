ab8f2915ef5334c71ce20ffb974605e8021826a0
AntonOshurek <oshurekweb@gmail.com>
2022-05-02 18:59:03 +0200

    add api code

---
+++ src/api-sevice.js
@@ -0,0 +1,51 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299,
+};
+
+export default class Api {
+  constructor(endPoint, autorization) {
+    this._endPoint = endPoint;
+    this._autorization = autorization;
+  }
+
+  getTasks() {
+    return this._load({url: 'movies'})
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Autorization', this._autorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static checkStatus(response) {
+    if(response.status < SuccessHTTPStatusRange.MIN ||
+    response.status > SuccessHTTPStatusRange.MAX) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+}
